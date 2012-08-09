0fc416e99d03877ca5b8dae0d5f50bb4db00bb4b
maxim <maxim@localhost.localdomain>
2012-08-10 00:55:53 +0300

    edit controller/log file

--- application/controllers/log.php
+++ application/controllers/log.php
@@ -1,6 +1,6 @@
 <?php
     class Log extends Controller{
-public $data = array();
+        public $data = array();
         
         
         function __construct(){
@@ -9,20 +9,12 @@ public $data = array();
         }
         
         function in(){
-$this->load->model('get_db');
-            
             if(isset($_POST['send'])){
                 $this->login = $_POST['login'];
                 $this->pass = Hash::create('md5', $_POST['password'], HASH_KEY);
-                
                 $tr = $this->get_db->select('admin', "`name` = '".$this->login."' AND `pass` = '".$this->pass."'");
                 
-                
-                
-                
-                
                 if(is_object($tr)){
-//                    $t = 'yes';
                 Session::init();
                 Session::set('login', 'admin');
                     header('Location: /');
@@ -30,28 +22,10 @@ $this->load->model('get_db');
                     $t = 'Wrong';
                 }
                 
-                // pass = 123123;
-                
-                
                 $this->data['post'] = $t;
             }else{
                 $this->data['post'] = NULL;
             }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
             $this->view('log/index', $this->data);
         }
         
@@ -59,7 +33,4 @@ $this->load->model('get_db');
             Session::destroy();
             header("Location: /");
         }
-        
-        
-        
     }
