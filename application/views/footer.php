3ec204aceac73c6c12b3950ee8ed82459ed77e13
Maxim <maxim@localhost.localdomain>
2012-08-10 12:26:57 +0400

    Add new style to footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,11 +2,13 @@
 <?php if(isset($_SESSION['login'])):?>
 <div id="footer">
     <div id="transparent"></div>
+    <label id="link">
     <a href="./">admin</a>
     <a href="./">radmin</a>
     <a href="./">ladmin</a>
     <a href="./">dadmin</a>
     <a href="./">fadmin</a>
+    </label>
 </div>
 <?php endif;?>
 </body>
