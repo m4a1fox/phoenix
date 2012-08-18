2b22b7faa3b4352f6e17c6d4b38f177d7f90d727
Maxim <maxim@localhost.localdomain>
2012-08-18 20:03:48 +0400

    Add new FIMA func

--- application/views/fima/js/init.js
+++ application/views/fima/js/init.js
@@ -8,12 +8,34 @@ $(document).ready(function(){
         data: {dir: '/application/'},
         success: function(data){
             $("#files").html(data)
+            
+            $("#files ul li").each(function(){
+                $(this).click(function(){
+                    var folder = $(this).children('a').text()
+                    $.ajax({
+                        type: "POST",
+                        url: "usefull/fima/home.php",
+                        data: {dir: '/application/'+folder+'/'},
+                        success: function(data){
+                            console.log(data)
+                        }
+                        
+                        
+                        
+                    })
+                })
+            })
         }
         
         
         
         
     })
+    
+    
+    
+    
+    
     
     
     
